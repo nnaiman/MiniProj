d70153a82976abb2062c3fbc7861b01cbaf10a92
nnaiman <nnaiman@g.jct.ac.il>
2021-04-11 22:50:16 +0300

    tests-finish

--- project/src/unitTests/CylinderTest.java
+++ project/src/unitTests/CylinderTest.java
@@ -11,10 +11,10 @@ class CylinderTest {
 
     @Test
     void getNormal() {
-        Assert.assertTrue("get normal cylinder",new Cylinder(new Ray(Point3D.ZERO,new Vector(0,0,1)),6,10).getNormal(new Point3D(0,6,5)).equals(new Vector(0,1,0)));
-        Assert.assertTrue("get normal cylinder",new Cylinder(new Ray(Point3D.ZERO,new Vector(0,0,1)),6,10).getNormal(new Point3D(0,5,10)).equals(new Vector(0,1,0)));
-        Assert.assertTrue("get normal cylinder",new Cylinder(new Ray(Point3D.ZERO,new Vector(0,0,1)),6,10).getNormal(new Point3D(0,5,0)).equals(new Vector(0,1,0)));
-        Assert.assertTrue("get normal cylinder",new Cylinder(new Ray(Point3D.ZERO,new Vector(0,0,1)),6,10).getNormal(new Point3D(0,0,0)).equals(new Vector(0,1,0)));
-        Assert.assertTrue("get normal cylinder",new Cylinder(new Ray(Point3D.ZERO,new Vector(0,0,1)),6,10).getNormal(new Point3D(0,0,10)).equals(new Vector(0,1,0)));
+        Assert.assertTrue("get normal cylinder1",new Cylinder(new Ray(Point3D.ZERO,new Vector(0,0,1)),6,10).getNormal(new Point3D(0,6,5)).equals(new Vector(0,1,0)));
+        Assert.assertTrue("get normal cylinder2",new Cylinder(new Ray(Point3D.ZERO,new Vector(0,0,1)),6,10).getNormal(new Point3D(0,5,10)).equals(new Vector(0,0,1)));
+        Assert.assertTrue("get normal cylinder3",new Cylinder(new Ray(Point3D.ZERO,new Vector(0,0,1)),6,10).getNormal(new Point3D(0,5,0)).equals(new Vector(0,0,-1)));
+        Assert.assertTrue("get normal cylinder4",new Cylinder(new Ray(Point3D.ZERO,new Vector(0,0,1)),6,10).getNormal(new Point3D(0,0,0)).equals(new Vector(0,0,-1)));
+        Assert.assertTrue("get normal cylinder5",new Cylinder(new Ray(Point3D.ZERO,new Vector(0,0,1)),6,10).getNormal(new Point3D(0,0,10)).equals(new Vector(0,0,1)));
     }
 }
